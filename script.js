786ec727ff8e213a7549035a8eae8475f9a1c5be
ryanj89 <ryanj89@gmail.com>
2017-01-25 13:34:23 -0700

    Checkerboard done

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Your JS goes here
+var body = document.querySelector('body');
+
+for (var i = 1; i <= 63; i++) {
+  var div = document.createElement('div');
+  div.style.width = '11.1%';
+  div.style.paddingBottom = '11.1%';
+  div.style.float = 'left';
+  if (i % 2) {
+    div.style.background = 'black';
+  } else {
+    div.style.background = 'red';
+  }
+  body.appendChild(div);
+}
